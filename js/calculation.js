17f2308ea503061fc27720e91b23f8849f5a1087
ssalvi8586 <ssalvi8586@gmail.com>
2022-02-17 16:31:46 +0600

    calculate expense and balance

--- js/calculation.js
+++ js/calculation.js
@@ -1,3 +1,4 @@
+// fetch value as number
 function amountValue(id) {
     const amount = parseFloat(document.getElementById(id).value);
     if (!isNaN(amount)) {
@@ -7,9 +8,29 @@ function amountValue(id) {
     }
 }
 
+// calculate total expense and balance 
+function calculate(income, food, rent, clothes) {
+    const expenses = food + rent + clothes;
+    const balance = income - expenses;
+
+    if (balance < 0) {
+        console.log("insufficient Balance");
+    } else {
+        document.getElementById("expenses").innerText = expenses;
+        document.getElementById("balance").innerHTML = balance;
+        return balance;
+    }
+}
+
+
+// click event 
 document.getElementById("calculate").addEventListener('click', function() {
-    income = amountValue("income");
-    income = amountValue("food");
-    income = amountValue("rent");
-    income = amountValue("clothes");
+    const income = amountValue("income");
+    const food = amountValue("food");
+    const rent = amountValue("rent");
+    const clothes = amountValue("clothes");
+
+    if (!isNaN(income) || income >= 0 || !isNaN(food) || food >= 0 || !isNaN(rent) || rent >= 0 || !isNaN(clothes) || clothes >= 0) {
+        const balance = calculate(income, food, rent, clothes);
+    }
 })
